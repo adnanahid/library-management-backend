de520ca76ab00d1a916f7205914eb9c14149f84f
Adnan <sikuahamed@gmail.com>
2026-05-01 22:50:39 +0600

    feat: add morgan for better understanding whats going on

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,13 @@ import express from 'express';
 import cors from 'cors';
 import routes from './routes/index.js';
 import { globalErrorHandler } from './middlewares/globalErrorHandler.js';
+import morgan from 'morgan';
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(morgan('dev'));
 
 app.use('/api', routes);
 
